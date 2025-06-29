b27379dc19a396cbf054284127351ecf2dd02c77
medicaresphere <medicaresphere@gmail.com>
2025-06-29 20:12:03 +0530

    Updated app-build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "edee4ac0bb9cbe98"; }
+/******/ 		__webpack_require__.h = function() { return "a3fc2790f5c91479"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
